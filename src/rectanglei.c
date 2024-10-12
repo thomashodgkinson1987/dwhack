0fbb821fe180d10354559f7741826f9cca09e2c5
Thomas Hodgkinson <thomashodgkinson1987@gmail.com>
2024-10-12 12:04:20 +0100

    Doc: Add comments to rectanglei

--- src/rectanglei.c
+++ src/rectanglei.c
@@ -1,5 +1,14 @@
 #include "rectanglei.h"
 
+/**
+ * @brief Creates a new rectanglei.
+ *
+ * @param x The x-coordinate of the top-left corner.
+ * @param y The y-coordinate of the top-left corner.
+ * @param w The width of the rectangle.
+ * @param h The height of the rectangle.
+ * @return struct rectanglei The created rectangle.
+ */
 struct rectanglei rectanglei_create(int x, int y, int w, int h)
 {
     struct rectanglei rectanglei =
@@ -12,8 +21,15 @@ struct rectanglei rectanglei_create(int x, int y, int w, int h)
 
     return rectanglei;
 }
+
+/**
+ * @brief Frees the memory allocated for a rectanglei (does nothing in this case, as it's a simple struct).
+ *
+ * @param rectanglei A pointer to the rectanglei to free.
+ */
 void rectanglei_free(struct rectanglei *rectanglei)
 {
+    //In this simple case, there is no dynamic memory allocation, so nothing needs to be freed.
     rectanglei->x = 0;
     rectanglei->y = 0;
     rectanglei->w = 0;
