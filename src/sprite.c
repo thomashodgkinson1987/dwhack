1030eeaf3491e4a547797a6e643d4d8b667f3d13
Thomas Hodgkinson <thomashodgkinson1987@gmail.com>
2024-10-12 19:17:30 +0100

    Refactor: Use sprite module functions
    Refactored game scene code to use functions from the sprite module for better organization and maintainability.
    Updated sprite module to use a more robust struct and added functions for easier sprite manipulation.

--- src/sprite.c
+++ src/sprite.c
@@ -2,103 +2,91 @@
 
 #include "raylib.h"
 
-struct sprite sprite_create(Texture2D texture, float x, float y, float w, float h)
-{
-    struct sprite sprite = {0};
+#include <stdlib.h>
 
-    sprite.is_visible = true;
-    sprite.texture = texture;
-    sprite.source = (Rectangle){0.0f, 0.0f, (float)texture.width, (float)texture.height};
-    sprite.dest = (Rectangle){x, y, w, h};
-    sprite.origin = (Vector2){0.0f, 0.0f};
-    sprite.rotation = 0.0f;
-    sprite.tint = WHITE;
+struct sprite_impl
+{
+    bool is_visible;
+    Texture2D texture;
+    Rectangle source;
+    Rectangle dest;
+    Vector2 origin;
+    float rotation;
+    Color tint;
+};
+
+Sprite *sprite_create(Texture2D texture, float x, float y, float w, float h)
+{
+    Sprite *sprite = (Sprite *)malloc(sizeof(struct sprite_impl));
+
+    if (sprite != NULL)
+    {
+        sprite->is_visible = true;
+        sprite->texture = texture;
+        sprite->source = (Rectangle){0.0f, 0.0f, (float)texture.width, (float)texture.height};
+        sprite->dest = (Rectangle){x, y, w, h};
+        sprite->origin = (Vector2){0.0f, 0.0f};
+        sprite->rotation = 0.0f;
+        sprite->tint = (Color){255, 255, 255, 255};
+    }
 
     return sprite;
 }
 
-void sprite_free(struct sprite *sprite)
+void sprite_free(Sprite *sprite)
 {
-    sprite->is_visible = 0;
-    sprite->texture = (Texture2D){0};
-    sprite->source = (Rectangle){0};
-    sprite->dest = (Rectangle){0};
-    sprite->origin = (Vector2){0};
-    sprite->rotation = 0.0f;
-    sprite->tint = BLANK;
+    if (sprite != NULL)
+    {
+        free(sprite);
+        sprite = NULL;
+    }
 }
 
-bool sprite_get_is_visible(struct sprite *sprite)
-{
-    return sprite->is_visible;
-}
+bool sprite_get_is_visible(const Sprite *sprite) { return sprite->is_visible; }
 
-Texture2D sprite_get_texture(struct sprite *sprite)
-{
-    return sprite->texture;
-}
+Texture2D sprite_get_texture(const Sprite *sprite) { return sprite->texture; }
 
-float sprite_get_x(struct sprite *sprite)
-{
-    return sprite->dest.x;
-}
-float sprite_get_y(struct sprite *sprite)
-{
-    return sprite->dest.y;
-}
-float sprite_get_w(struct sprite *sprite)
-{
-    return sprite->dest.width;
-}
-float sprite_get_h(struct sprite *sprite)
-{
-    return sprite->dest.height;
-}
+Rectangle sprite_get_source(const Sprite *sprite) { return sprite->source; }
 
-void sprite_set_is_visible(struct sprite *sprite, bool is_visible)
-{
-    sprite->is_visible = is_visible;
-}
+Rectangle sprite_get_dest(const Sprite *sprite) { return sprite->dest; }
 
-void sprite_set_texture(struct sprite *sprite, Texture2D texture)
-{
-    sprite->texture = texture;
-}
+Vector2 sprite_get_origin(const Sprite *sprite) { return sprite->origin; }
 
-void sprite_set_x(struct sprite *sprite, float x)
-{
-    sprite->dest.x = x;
-}
-void sprite_set_y(struct sprite *sprite, float y)
-{
-    sprite->dest.y = y;
-}
-void sprite_set_w(struct sprite *sprite, float w)
-{
-    sprite->dest.width = w;
-}
-void sprite_set_h(struct sprite *sprite, float h)
-{
-    sprite->dest.height = h;
-}
+float sprite_get_rotation(const Sprite *sprite) { return sprite->rotation; }
 
-void sprite_set_position(struct sprite *sprite, float x, float y)
-{
-    sprite_set_x(sprite, x);
-    sprite_set_y(sprite, y);
-}
-void sprite_set_size(struct sprite *sprite, float w, float h)
-{
-    sprite_set_w(sprite, w);
-    sprite_set_h(sprite, h);
-}
+Color sprite_get_tint(const Sprite *sprite) { return sprite->tint; }
 
-void sprite_toggle_is_visible(struct sprite *sprite)
-{
-    sprite->is_visible = !sprite->is_visible;
-}
+void sprite_set_is_visible(Sprite *sprite, bool is_visible) { sprite->is_visible = is_visible; }
 
-void sprite_draw(struct sprite *sprite)
+void sprite_set_texture(Sprite *sprite, Texture2D texture) { sprite->texture = texture; }
+
+void sprite_set_source(Sprite *sprite, Rectangle source) { sprite->source = source; }
+
+void sprite_set_dest(Sprite *sprite, Rectangle dest) { sprite->dest = dest; }
+
+void sprite_set_origin(Sprite *sprite, Vector2 origin) { sprite->origin = origin; }
+
+void sprite_set_rotation(Sprite *sprite, float rotation) { sprite->rotation = rotation; }
+
+void sprite_set_tint(Sprite *sprite, Color tint) { sprite->tint = tint; }
+
+float sprite_get_x(const Sprite *sprite) { return sprite->dest.x; }
+
+float sprite_get_y(const Sprite *sprite) { return sprite->dest.y; }
+
+float sprite_get_w(const Sprite *sprite) { return sprite->dest.width; }
+
+float sprite_get_h(const Sprite *sprite) { return sprite->dest.height; }
+
+void sprite_set_x(Sprite *sprite, float x) { sprite->dest.x = x; }
+
+void sprite_set_y(Sprite *sprite, float y) { sprite->dest.y = y; }
+
+void sprite_set_w(Sprite *sprite, float w) { sprite->dest.width = w; }
+
+void sprite_set_h(Sprite *sprite, float h) { sprite->dest.height = h; }
+
+void sprite_draw(const Sprite *sprite)
 {
     if (!sprite->is_visible)
     {
