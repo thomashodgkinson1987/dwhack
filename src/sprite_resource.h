048746f6c3054cbc0100a736023fc29ac2a538ac
Thomas Hodgkinson <thomashodgkinson1987@gmail.com>
2024-10-15 13:50:15 +0100

    feat: initial implementation of sprite resource

---
+++ src/sprite_resource.h
@@ -0,0 +1,20 @@
+#ifndef SPRITE_RESOURCE_H
+#define SPRITE_RESOURCE_H
+
+#include "sprite.h"
+
+typedef struct sprite_resource_impl SpriteResource;
+
+SpriteResource *sprite_resource_create(void);
+
+void sprite_resource_free(SpriteResource *sprite_resource);
+
+char *sprite_resource_get_name(SpriteResource *sprite_resource);
+
+Sprite *sprite_resource_get_sprite(SpriteResource *sprite_resource);
+
+void sprite_resource_set_name(SpriteResource *sprite_resource, char *name);
+
+void sprite_resource_set_sprite(SpriteResource *sprite_resource, Sprite *sprite);
+
+#endif
